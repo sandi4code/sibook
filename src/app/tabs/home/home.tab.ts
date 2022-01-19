6d2903bb43582a81fa5b354f63ceea0dbc452650
Sandi Anjar Maulana <sandi4code@gmail.com>
2022-01-19 10:50:28 +0700

    styling home

--- src/app/tabs/home/home.tab.ts
+++ src/app/tabs/home/home.tab.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-home-tab',
   templateUrl: 'home.tab.html',
-  styleUrls: ['home.tab.scss']
+  styleUrls: ['home.tab.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomeTab {
 
