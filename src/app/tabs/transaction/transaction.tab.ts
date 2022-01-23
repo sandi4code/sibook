70d20c265d58c4820cb3a2253a6ddee64dffbd91
Sandi Anjar Maulana <sandi4code@gmail.com>
2022-01-23 18:10:40 +0700

    transaction page

--- src/app/tabs/transaction/transaction.tab.ts
+++ src/app/tabs/transaction/transaction.tab.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-transaction-tab',
   templateUrl: 'transaction.tab.html',
-  styleUrls: ['transaction.tab.scss']
+  styleUrls: ['transaction.tab.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TransactionTab {
 
